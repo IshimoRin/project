7c15fb377812f3724940f469df7ce58ab0af2430
IshimoRin <90213193+IshimoRin@users.noreply.github.com>
2024-05-17 14:45:36 +0300

    Delete scripts directory

--- scripts/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer();
-
-server.on('request', (req, res) => {
-    res.end('');
-});
-
-server.listen(3000, () => console.log('server works'));
